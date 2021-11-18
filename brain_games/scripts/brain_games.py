af1423120149fae90d982f43f47b13fdc9837c59
AkmAZ <alien1386@mail.ru>
2021-11-18 19:47:36 +0500

    Three

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello Hexlet BrainGames")
+from brain_games.cli import welcome_user
 
+def main():
+    print("Hello, %s!" % name)
+
+name = welcome_user()
 
 if __name__ == '__main__':
     main()
