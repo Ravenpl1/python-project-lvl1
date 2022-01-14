e8862e780170234fca881b8111dd3c9a54798afa
Azmont <alien1386@mail.ru>
2022-01-14 09:31:26 +0300

    WPS

--- tests/test_brain_games.py
+++ tests/test_brain_games.py
@@ -1,5 +1,10 @@
-import brain_games
+# -*- coding: utf-8 -*- #
+
+"""Функция игры brain-progression."""
 
 
 def test_brain_games():
-    assert 1 == 1
+    """Функция игры brain-progression."""
+    temp1 = 1
+    temp2 = 1
+    assert temp1 == temp2
