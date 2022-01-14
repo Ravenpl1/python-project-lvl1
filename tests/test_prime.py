17d47a18bce18c52e0ede922778e5917631a0468
Azmont <alien1386@mail.ru>
2022-01-14 12:25:03 +0300

    tests

---
+++ tests/test_prime.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*- #
+
+"""Импорт brain_prime."""
+from brain_games.games import prime
+
+
+def test_gcd():
+    """Функция теста игры brain_prime."""
+    generate_data = tuple(prime.brain_prime().keys())
+    assert generate_data == ('correct_answer', 'rule', 'question')
